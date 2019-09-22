59386fedd9382f9f71be90e17213c5465aa1ab26
Alexey <44061702+samarec1812@users.noreply.github.com>
2019-09-23 02:55:46 +0300

    Update fibonacci-number on O(n).cpp

--- fibonacci-number on O(n).cpp
+++ fibonacci-number on O(n).cpp
@@ -1,15 +1,16 @@
+//https://leetcode.com/problems/fibonacci-number/
 class Solution {
 public:
     int fib(int N) {
        if(N <2) return N; 
         else if(N ==2 ) return 1; 
-        int m = 1 ,n =1, chlen =0; 
+        int m = 1 ,n =1, chl =0; 
          for(int i =2; i < N; ++i){
-            chlen = m+n; 
+            chl = m+n; 
              m = n; 
-             n = chlen; 
+             n = chl; 
              
          }
-        return chlen; 
+        return chl; 
     }
 };
